0eb1f1a626a9d54d25df99e0714fc1dbfd062001
Sarosh Farhan <saroshfarhan6495@gmail.com>
2023-01-21 21:06:45 +0530

    card design completed.

--- src/components/cardPreview/cardPreview.jsx
+++ src/components/cardPreview/cardPreview.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import styl from "./cardPreview.module.scss";
+import cardLogo from "../../assets/card-logo.svg";
 
 function CardPreview() {
   return (
-    <div className="card-preview-container">
+    <div className={styl.cardPreviewContainer}>
       <div className={styl.cardFront}>
-        <img src="" alt="card logo" />
+        <div className={styl.cardDetails}>
+          <img src={cardLogo} alt="card logo" />
+          <p className={styl.name}>0000 0000 0000 0000</p>
+          <div className={styl.cardInfo}>
+            <p>Jane Appleseed</p>
+            <p>00/00</p>
+          </div>
+        </div>
       </div>
-      <div className={styl.cardBack}></div>
+      <div className={styl.cardBack}>
+        <p>000</p>
+      </div>
     </div>
   );
 }
